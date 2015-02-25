046fe3b5d0f1f0e3e3abb0a9273f39d52f0814c7
Sina Rostam <Sinarostam@icloud.com>
2015-02-25 15:01:57 -0800

    validates for name is taken out

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  validates :name, presence: true
+  #validates :name, presence: true
 
   has_many :listings, dependent: :destroy   #lising will be automatically deleted 
 end
+
