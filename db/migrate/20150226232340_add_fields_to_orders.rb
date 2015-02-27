cb26d94e936ca609ab1cfa00b708831de9c0da1b
Sina Rostam <Sinarostam@icloud.com>
2015-02-27 12:43:43 -0800

    integer is fixed

--- db/migrate/20150226232340_add_fields_to_orders.rb
+++ db/migrate/20150226232340_add_fields_to_orders.rb
@@ -1,6 +1,6 @@
 class AddFieldsToOrders < ActiveRecord::Migration
   def change
-    add_column :orders, :buyer_id, :intege
-    add_column :orders, :seller_id, :integerr
+    add_column :orders, :buyer_id, :integer
+    add_column :orders, :seller_id, :integer
   end
 end
