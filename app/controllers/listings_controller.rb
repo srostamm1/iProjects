31c94cc87e7f195b7102d5ded48391d4bf42d11c
Sina Rostam <Sinarostam@icloud.com>
2015-06-24 06:59:15 -0700

    listing controller updated

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,13 +2,13 @@ class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
-  # GET /listings
-  # GET /listings.json
+
   def seller
     @listings = Listing.where(user: current_user).order("created_at DESC")
   end
 
-
+  # GET /listings
+  # GET /listings.json
   def index
     @listings = Listing.all.order("created_at DESC")
   end
@@ -16,7 +16,6 @@ class ListingsController < ApplicationController
   # GET /listings/1
   # GET /listings/1.json
   def show
-    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/new
@@ -31,11 +30,10 @@ class ListingsController < ApplicationController
   # POST /listings
   # POST /listings.json
   def create
-    @listings = Listing.new(listing_params)
-    @listings.user_id = current_user.id
-     # this command is going set the user id to this listing to current id of the user ++
+    @listing = Listing.new(listing_params)
+    @listing.user_id = current_user.id
+
     if current_user.recipient.blank?
-      require "stripe"
       Stripe.api_key = ENV["STRIPE_API_KEY"]
       token = params[:stripeToken]
 
@@ -44,16 +42,17 @@ class ListingsController < ApplicationController
         :type => "individual",
         :bank_account => token
         )
+
+      current_user.recipient = recipient.id
+      current_user.save
     end
-    current_user.recipient = recipient.id
-    current_user.save
 
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
-        format.json { render :show, status: :created, location: @listing }
+        format.json { render action: 'show', status: :created, location: @listing }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -65,9 +64,9 @@ class ListingsController < ApplicationController
     respond_to do |format|
       if @listing.update(listing_params)
         format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -78,7 +77,7 @@ class ListingsController < ApplicationController
   def destroy
     @listing.destroy
     respond_to do |format|
-      format.html { redirect_to listings_url, notice: 'Listing was successfully Removed.' }
+      format.html { redirect_to listings_url }
       format.json { head :no_content }
     end
   end
@@ -95,8 +94,8 @@ class ListingsController < ApplicationController
     end
 
     def check_user
-      if current_user!= @listing.user
-        redirect_to root_url, alert: "sorry, this listing belongs to someone else"
+      if current_user != @listing.user
+        redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
       end
     end
 end
