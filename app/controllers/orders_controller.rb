93a0742804a5a900ea9a6e7eafc91bb8084cb080
Sina Rostam <Sinarostam@icloud.com>
2015-06-24 07:13:59 -0700

    order controller added the success message, A.Y.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,22 @@
+
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  # GET /orders
-  # GET /orders.json
-  
-  def sales 
+
+  def sales
     @orders = Order.all.where(seller: current_user).order("created_at DESC")
   end
 
   def purchases
-      @orders = Order.all.where(buyer: current_user).order("created_at DESC")
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
   end
-
 
   # GET /orders/new
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
-
   end
+
   # POST /orders
   # POST /orders.json
   def create
@@ -33,15 +31,14 @@ class OrdersController < ApplicationController
     Stripe.api_key = ENV["STRIPE_API_KEY"]
     token = params[:stripeToken]
 
-    begin 
+    begin
       charge = Stripe::Charge.create(
         :amount => (@listing.price * 100).floor,
         :currency => "usd",
         :card => token
         )
-      flash[:notice] = "Thanks for Ordering!"
     rescue Stripe::CardError => e
-        flash[:danger] = e.message
+      flash[:danger] = e.message
     end
 
     transfer = Stripe::Transfer.create(
@@ -52,10 +49,10 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to root_url }
-        format.json { render :show, status: :created, location: @order }
+        format.html { redirect_to root_url, notice: "Thanks for ordering!" }
+        format.json { render action: 'show', status: :created, location: @order }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
